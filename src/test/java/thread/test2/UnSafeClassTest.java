d0f71b0e484fc996798e8c58616ec695f03493e0
Joby JB4 Hu <hujb4@lenovo.com>
2017-12-12 15:13:16 +0800

    modify pom skip unit test and comment unsafe code

--- src/test/java/thread/test2/UnSafeClassTest.java
+++ src/test/java/thread/test2/UnSafeClassTest.java
@@ -1,20 +1,20 @@
-package thread.test2;
-
-import sun.misc.Unsafe;
-
-import java.lang.reflect.Field;
-
-public class UnSafeClassTest {
-
-    private int i = 0;
-
-    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
-        Field f = Unsafe.class.getDeclaredField("theUnsafe");
-        f.setAccessible(true);
-        Unsafe unsafe = (Unsafe) f.get(null);
-        long offset = unsafe.objectFieldOffset(UnSafeClassTest.class.getDeclaredField("i"));
-        UnSafeClassTest unSafeClassTest = new UnSafeClassTest();
-        unsafe.putInt(unSafeClassTest, offset, 100);
-        System.out.println(unSafeClassTest.i);
-    }
-}
+//package thread.test2;
+//
+//import sun.misc.Unsafe;
+//
+//import java.lang.reflect.Field;
+//
+//public class UnSafeClassTest {
+//
+//    private int i = 0;
+//
+//    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+//        Field f = Unsafe.class.getDeclaredField("theUnsafe");
+//        f.setAccessible(true);
+//        Unsafe unsafe = (Unsafe) f.get(null);
+//        long offset = unsafe.objectFieldOffset(UnSafeClassTest.class.getDeclaredField("i"));
+//        UnSafeClassTest unSafeClassTest = new UnSafeClassTest();
+//        unsafe.putInt(unSafeClassTest, offset, 100);
+//        System.out.println(unSafeClassTest.i);
+//    }
+//}
--- src/test/java/thread/test2/UnsafeDemo.java
+++ src/test/java/thread/test2/UnsafeDemo.java
@@ -1,36 +1,36 @@
-package thread.test2;
-
-import sun.misc.Unsafe;
-
-import java.lang.reflect.Field;
-
-public class UnsafeDemo {
-    public static void main(String[] args) throws Exception{
-        Field f = Unsafe.class.getDeclaredField("theUnsafe"); // Internal reference
-        f.setAccessible(true);
-        Unsafe unsafe = (Unsafe) f.get(null);
-
-        // This creates an instance of player class without any initialization
-        Player p = (Player) unsafe.allocateInstance(Player.class);
-        System.out.println(p.getAge()); // Print 0
-
-        p.setAge(45); // Let's now set age 45 to un-initialized object
-        System.out.println(p.getAge()); // Print 45
-    }
-}
-
-class Player {
-    private int age = 12;
-
-    public Player() {
-        this.age = 50;
-    }
-
-    public int getAge() {
-        return this.age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-}
+//package thread.test2;
+//
+//import sun.misc.Unsafe;
+//
+//import java.lang.reflect.Field;
+//
+//public class UnsafeDemo {
+//    public static void main(String[] args) throws Exception{
+//        Field f = Unsafe.class.getDeclaredField("theUnsafe"); // Internal reference
+//        f.setAccessible(true);
+//        Unsafe unsafe = (Unsafe) f.get(null);
+//
+//        // This creates an instance of player class without any initialization
+//        Player p = (Player) unsafe.allocateInstance(Player.class);
+//        System.out.println(p.getAge()); // Print 0
+//
+//        p.setAge(45); // Let's now set age 45 to un-initialized object
+//        System.out.println(p.getAge()); // Print 45
+//    }
+//}
+//
+//class Player {
+//    private int age = 12;
+//
+//    public Player() {
+//        this.age = 50;
+//    }
+//
+//    public int getAge() {
+//        return this.age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//}
